31423b4edc1971bdeb7468f1fbc4b835a81e4192
Bada1208 <sysoievbogdan@gmail.com>
2020-05-31 20:55:54 +0200

    added bean lifecycle sample

---
+++ 4_bean_lifecycle/src/com/sysoiev/lifecycle/MessageRunner.java
@@ -0,0 +1,14 @@
+package com.sysoiev.lifecycle;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MessageRunner {
+    public static void main(String[] args) {
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("message-bean.xml");
+        Message message = (Message) context.getBean("printMessage");
+        System.out.println(message.getMessage());
+        context.registerShutdownHook();
+
+    }
+}
