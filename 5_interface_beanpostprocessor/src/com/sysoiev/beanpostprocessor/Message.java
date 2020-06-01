ac6a04947918ad1c46fb1f9e3f16c719a78c37f3
Bada1208 <sysoievbogdan@gmail.com>
2020-06-01 06:52:16 +0200

    added beanpostprocessor sample

---
+++ 5_interface_beanpostprocessor/src/com/sysoiev/beanpostprocessor/Message.java
@@ -0,0 +1,21 @@
+package com.sysoiev.beanpostprocessor;
+
+public class Message {
+    private String message;
+
+    public String getMessage() {
+        return "Message: " + message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void beanInit() {
+        System.out.println("Bean is passing init stage");
+    }
+
+    public void beanDestroy() {
+        System.out.println("Bean is passing destroy stage");
+    }
+}
