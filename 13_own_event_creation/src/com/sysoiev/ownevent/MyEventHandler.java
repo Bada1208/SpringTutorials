44802c1aafbdfefd318035346c5e5196227b068a
Bada1208 <sysoievbogdan@gmail.com>
2020-06-05 07:16:49 +0200

    added spring mvc

---
+++ 13_own_event_creation/src/com/sysoiev/ownevent/MyEventHandler.java
@@ -0,0 +1,16 @@
+package com.sysoiev.ownevent;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
+
+public class MyEventHandler implements ApplicationListener {
+
+    public void onApplicationEvent(MyEvent myEvent) {
+        System.out.println(myEvent.toString());
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent applicationEvent) {
+
+    }
+}
---
+++ 13_own_event_creation/src/com/sysoiev/ownevent/MyEventRunner.java
@@ -0,0 +1,15 @@
+package com.sysoiev.ownevent;
+
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MyEventRunner {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                new ClassPathXmlApplicationContext("my-event-config.xml");
+
+        MyEventPublisher myEventPublisher =
+                (MyEventPublisher) context.getBean("myEventPublisher");
+        myEventPublisher.publishEvent();
+    }
+}
